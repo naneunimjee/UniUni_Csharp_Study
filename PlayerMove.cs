73b6fdaf45bb341ac95eb032d9f364eb05122922
임지영 <141422840+naneunimjee@users.noreply.github.com>
2023-10-20 00:13:38 +0900

    Add jumping function
    1. Add jumping animation
    2. Add Raycast function to detect platform and animation transition
    3. Modify Layer so that Raycast can detect platform (ignore player layer)
    4. Add condition to prevent infinite jumping

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer ;
     Animator animator;
@@ -18,28 +19,43 @@ public class PlayerMove : MonoBehaviour
     }
 
     //Stop Speed
-    void Update() //지속적으로 호출, 보통 단발적인 키 입력을 다룰 때 사용
+    void Update() //지속적으로 호출, 보통 **단발적인 키 입력**을 다룰 때 사용
     {
+
+        
+    //jump
+
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower , ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+        
+    
         if(Input.GetButtonUp("Horizontal"))
         {  
             //rigid.velocity.x 는 방향뿐만아니라 크기까지 포함하는 표현
             //크기를 단위로 만들어주어야함.
             //normalized 를 쓰면 벡터 크기를 1로 만들 수 있음.
-            //GetAxisRaw와 거의 비슷..
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f , rigid.velocity.y);
+            //GetAxisRaw와 거의 비슷
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.7f , rigid.velocity.y);
         }
 
         //direction change
         if(Input.GetButtonDown("Horizontal"))
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1 ;
+            //Input.GetAxisRaw("Horizontal")==-1이 성립하면 true가 반환
         }
 
         //Animation transition
         if(Mathf.Abs(rigid.velocity.x) < 0.5)
             animator.SetBool("isWalking",false);
+
         else
             animator.SetBool("isWalking",true);
+        
+    
     }
 
     //Max Speed
@@ -64,5 +80,20 @@ public class PlayerMove : MonoBehaviour
     
     }
 
+    //Landing Platform
+    if (rigid.velocity.y < 0){
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform") );
+
+        if (rayHit.collider != null)
+        {
+            if (rayHit.distance < 0.5f){
+                Debug.Log(rayHit.collider.name);
+                animator.SetBool("isJumping",false);
+            }
+        }
+
+        }
     }
+
 }
