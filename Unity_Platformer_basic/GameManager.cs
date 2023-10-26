94307d38c03a2961b7571f75195a39dd00ef21ce
임지영 <naneunimjee1014@naver.com>
2023-10-27 00:34:01 +0900

    Modify GameManager.cs
    1. Make UI using UnityEngine.UI, TMPro, UnityEngine.SceneManagement namespaces
    2. Make retry button when the game was ended.
    3. Add PlayerReposition function, when player was falling or game was restarted
    4. Make player's health UI, stage level UI, total point UI

--- Unity_Platformer_basic/GameManager.cs
+++ Unity_Platformer_basic/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,13 +12,48 @@ public class GameManager : MonoBehaviour
     public int stageIndex;
     public int health;
     public PlayerMove playerMove;
+    public GameObject[] Stages;
 
+    public Image[] UIhealth;
+    public TextMeshProUGUI UItotal;
+    public TextMeshProUGUI UIstage;
+    public GameObject RestartBtn;
+
+    public void Update()
+    {
+        UItotal.text = (totalPoint + stagePoint).ToString();
+        UIstage.text = "Stage" + (stageIndex + 1).ToString();
+    }
     public void NextStage()
     {
-        stageIndex++;
+        if (stageIndex < Stages.Length - 1)
+        {
+            //Change Stage
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+
+            PlayerReposition();
+
+            UIstage.text = "Stage" + (stageIndex + 1).ToString();
+        }
+
+        else //Stage Clear
+        {
+            //player control lock
+            Time.timeScale = 0;
+
+            //Retry?
+            RestartBtn.SetActive(true);
+            TextMeshProUGUI BtnText = RestartBtn.GetComponentInChildren<TextMeshProUGUI>();
+            BtnText.text = "Game Clear";
+        }
+
 
         totalPoint += stagePoint;
         stagePoint = 0;
+
+        Debug.Log("다음스테이지로");
 
     }
 
@@ -23,17 +61,18 @@ public class GameManager : MonoBehaviour
     {
         if (collision.gameObject.name == "player")
         {
+            if (health > 1)
+            {
+                //Player Reposition
+                PlayerReposition();
+            }
+
+            //UI Image Delete
+            UIhealth[health - 1].color = new Color(1, 1, 1, 0);
+
             //Health down
             HealthDown();
 
-            //Player Reposition
-            collision.attachedRigidbody.velocity = Vector2.zero;
-            collision.transform.position = new Vector3(3, 1, 0);
-
-            if (health == 0)
-            {
-                Debug.Log("게임오버");
-            }
         }
     }
 
@@ -41,10 +80,38 @@ public class GameManager : MonoBehaviour
     public void HealthDown()
     {
         if (health > 1)
+        {
             health--;
+            //UI Image Delete
+            UIhealth[health].color = new Color(1, 1, 1, 0);
+        }
         else
+        {
+            health--;
+            UIhealth[health].color = new Color(1, 1, 1, 0);
+            //Player Die Effect
             playerMove.OnDie();
 
+            //Result UI 
+            Debug.Log("게임오버");
+
+            //Retry Button UI
+            RestartBtn.SetActive(true);
+        }
+
+    }
+
+    void PlayerReposition()
+    {
+        //Player Reposition
+        playerMove.VelocityZero();
+        playerMove.transform.position = new Vector3(3, 1, 0);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
 
 }
